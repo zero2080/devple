6d69cb84cc2f4ce8eb630a4332fc2aeb3256ef6c
zero2080 <zero2080@naver.com>
2023-12-21 14:41:59 +0900

    [수정]시작 클래그 이름 변경
      - api-server
      - auth-server
      - message-server

--- auth-server/src/main/java/net/devple/AuthenticationServerApplication.java
+++ auth-server/src/main/java/net/devple/AuthenticationServerApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthenticationApplication {
+public class AuthenticationServerApplication {
   public static void main(String[] args) {
-    SpringApplication.run(AuthenticationApplication.class,args);
+    SpringApplication.run(AuthenticationServerApplication.class,args);
   }
 }
--- message-server/src/main/java/net/devple/message/MessageServerApplication.java
+++ message-server/src/main/java/net/devple/message/MessageServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProducerServerApplication {
+public class MessageServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProducerServerApplication.class, args);
+		SpringApplication.run(MessageServerApplication.class, args);
 	}
 
 }
